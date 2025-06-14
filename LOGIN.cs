6a8f3cc9c603f0f87d6f4e608297797cf9ab8184
Jasmine1029 <jasmineaserafin@iskolarngbayan.pup.edu.ph>
2025-06-14 19:22:14 +0800

    Create LOGIN.cs

---
+++ LOGIN.cs
@@ -0,0 +1,37 @@
+using POSapp;
+
+namespace POSapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxUsernameLogin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelUsernameLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkSignUp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPasswordLogin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
